69a5ca56dac545cc3c66bb56a5c6049d1757ccc7
Banjoko Azeez <banjokoazeez@banjokos-mbp.home>
2022-01-21 22:35:46 +0000

    Implemented edit and update functionality

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   describe 'Creation' do
     before do
-      user = User.create(email: 'test@test.com', password: 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'azeez', last_name: 'banjoko')
-      @post = Post.create(date: Date.today, rationale: 'Anything can be here', user_id: user.id)
+      @post = FactoryBot.create(:post)
     end
     it 'can be created' do
       expect(@post).to be_valid
