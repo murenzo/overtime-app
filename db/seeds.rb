9505bfd69025d2895db80b4451b3debab1e9425c
Banjoko Azeez <banjokoazeez@banjokos-mbp.home>
2022-01-29 22:53:07 +0000

    Implemented edit functionality for status

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,9 @@
-user = User.create(email: 'test@test.com', password: 'password', password_confirmation: 'password', first_name: 'azeez', last_name: 'banjoko')
+user = User.create(email: 'test@test.com', password: 'password123', password_confirmation: 'password123', first_name: 'test', last_name: 'account')
+
+AdminUser.create(email: 'azeez@test.com', password: 'password123', password_confirmation: 'password123', first_name: 'azeez', last_name: 'banjoko')
+
+puts '1 admin user created'
+
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: user.id)
 end
